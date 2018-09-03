04175c8dd78f6b8a81b69c90503d18d89744c85f
ZayneFrauendorf <zaynef@outlook.com>
2018-09-03 18:12:42 +1200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+
+window.onload = function () {
+  var url = window.location.href;
+
+  if (url.match(/index/g)) {
+    const nodelist = document.querySelectorAll('#nav-item');
+    nodelist.forEach((element) => {
+      element.innerHTML === "Home" ? element.style.color = "red" : element.style.color = "black"
+    });
+
+    var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2900,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+  } else if (url.match(/contact/g)) {
+    const nodelist = document.querySelectorAll('#nav-item');
+    nodelist.forEach((element) => {
+      element.innerHTML === "Contact" ? element.style.color = "red" : element.style.color = "black"
+    })
+  }
+
+}
+
