fd0cd4523477482fe6ac325d0e616c3c5fbc008e
ZayneFrauendorf <zaynef@outlook.com>
2018-09-18 12:18:21 +1200

    fixes/wording changes and infoModule created for ease in future

--- index.js
+++ index.js
@@ -1,12 +1,91 @@
 
-window.onload = function () {
+
+var infoModule = (function () {
+  let email = 'accounts@drsvsd.co.nz';
+  let phone = '0800 377 873';
+
+  let aucklandStreetAddress = 'Gate 7, Unit 786 Jarvis Way';
+  let aucklandSuburbAndCity = 'East Tamaki, Auckland';
+
+  let christchurchStreetAddress = '5 Vulcan Place';
+  let christchurchSuburbAndCity = 'Middleton, Christchurch';
+
+  let correctYear = new Date().getFullYear();
+
+  let copyrightInfo = `&copy; ${correctYear} - Drive Repair Shop`
+
+  function copyrightInfoProvided() {
+    let copyrightElement = document.querySelector('footer');
+    copyrightElement.innerHTML = copyrightInfo;
+    return;
+  }
+
+  function christchurchAddressProvided() {
+    let christchurchAddress = document.querySelector('.christchurch-address');
+    christchurchAddress.innerHTML = `${christchurchStreetAddress} </br> ${christchurchSuburbAndCity}`
+    return;
+  }
+
+  function aucklandAddressProvided() {
+    let aucklandAddress = document.querySelector('.auckland-address');
+    aucklandAddress.innerHTML = `${aucklandStreetAddress} </br> ${aucklandSuburbAndCity}`
+    return;
+  }
+
+  function emailProvided() {
+    let emailItemList = document.querySelectorAll('.email');
+
+    for (let item of emailItemList) {
+      item.href = `mailto:${email}`;
+      item.textContent = email;
+    }
+    return;
+  }
+
+  function phoneProvided() {
+    let phoneItemList = document.querySelectorAll('.phone');
+
+    for (let item of phoneItemList) {
+      item.href = `tel:${phone.replace(/\s/g, "")}`;
+      item.textContent = phone;
+    }
+    return;
+  }
+  return {
+    emailProvided: emailProvided,
+    phoneProvided: phoneProvided,
+    christchurchAddressProvided: christchurchAddressProvided,
+    aucklandAddressProvided: aucklandAddressProvided,
+    copyrightInfoProvided: copyrightInfoProvided
+  };
+})();
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
   var url = window.location.href;
+  infoModule.emailProvided();
+  infoModule.phoneProvided();
+  infoModule.copyrightInfoProvided();
 
   if (url.match(/contact/g)) {
+
+    infoModule.christchurchAddressProvided();
+    infoModule.aucklandAddressProvided();
+
     const nodelist = document.querySelectorAll('#nav-item');
     nodelist.forEach((element) => {
       element.innerHTML === "Contact" ? element.style.color = "red" : element.style.color = "black"
     })
+
+    if (window.innerWidth > 480) {
+      let contactColOneAndThree = document.querySelectorAll('.contact-col:nth-child(odd)');
+      let addition = `<p></p><p></p><p></p>`;
+
+      for (let item of contactColOneAndThree) {
+        item.innerHTML = item.innerHTML + addition;
+      }
+    }
 
   } else {
     const nodelist = document.querySelectorAll('#nav-item');
@@ -30,7 +109,6 @@ window.onload = function () {
         prevEl: '.swiper-button-prev',
       },
     });
-  }
-
-}
+  };
+});
 
